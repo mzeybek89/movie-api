f290e265cfde90c83e4d5863865000ee3ecc06d2
Mehmet <mehmetzeybek89@gmail.com>
2018-12-27 13:30:15 +0300

    api/movies

--- app.js
+++ app.js
@@ -2,27 +2,28 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const bodyParser  = require('body-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const movieRouter = require('./routes/movie');
 
 const app = express();
+
+// db connection
+const db = require('./helpers/db')();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/movie', movieRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
