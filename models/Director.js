ae0201913ee669f8c3710af44cf477c642a9bab4
mzeybek89 <mehmetzeybek89@gmail.com>
2018-12-27 20:26:31 +0300

    Initial Commit

---
+++ models/Director.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DirectorSchema = new Schema({
+    name:{
+        type: String,
+        required: [true,"{PATH} alanı Zorunludur."]
+    },
+    surname:{
+        type: String,
+        required: [true,"{PATH} alanı Zorunludur."]
+    },
+    bio: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+
+module.exports = mongoose.model('director', DirectorSchema);
